0c96d709906bfc457ecb2e34637c10a1c89e2089
Luis Fernando <luis.betancourt@ank.app>
2022-07-11 09:35:03 -0300

    Initial Commit

---
+++ gaming-profile-search/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  gaming-profile-search
+//
+//  Created by luis.betancourt on 11/07/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ gaming-profile-search/gaming_profile_searchApp.swift
@@ -0,0 +1,17 @@
+//
+//  gaming_profile_searchApp.swift
+//  gaming-profile-search
+//
+//  Created by luis.betancourt on 11/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct gaming_profile_searchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
